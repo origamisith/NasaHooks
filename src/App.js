fa61bb6480d5f6bf7752ba1dae0555cf1428c0dc
Luke Sedillo <SedilloL@cdk.com>
2021-06-25 08:42:56 -0700

    Added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useRef, useState} from "react";
+import {useQuery, useQueryClient} from "react-query";
+import dotenv from "dotenv";
 
+const NASA_KEY=''; //See https://api.nasa.gov/
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [index, setIndex] = useState(0);
+    const [day, setDay] = useState(22);
+    const [playing, setPlaying] = useState(false);
+    const [imgLoaded, setImgLoaded] = useState(false);
+    const {loading, data, isLoading, isSuccess, isError, error,isFetching } = useQuery(['rover', day], () => {
+        return fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/photos' +
+            '?earth_date=2021-06-'+day+
+            '&api_key=' + NASA_KEY).then(response => response.json())
+    })
+
+    // useInterval(() => {
+    //     if(playing && isSuccess && !isLoading && !isFetching && imgLoaded) {
+    //         setImgLoaded(false);
+    //         setIndex((index + 1) % data.photos.length)
+    //     }
+    // }, 100);
+    useEffect(() => {
+        const timer = window.setInterval(() => {
+            if(playing && imgLoaded) {
+                setImgLoaded(false);
+                setIndex((index + 1) % data.photos.length)
+            }
+        }, 100)
+        return () => {
+            window.clearInterval(timer);
+        }
+    })
+
+
+    return (
+        <div className="App">
+            <button onClick={() => setPlaying(!playing)}>Play/Pause</button>
+            <button onClick ={() => setIndex((index-1 + data.photos.length)%data.photos.length)}>Previous</button>
+            <button onClick ={() => setIndex((index+1)%data.photos.length)}>Next</button>
+            <button onClick ={() => {
+                setDay ((day-1 + 30)%30)
+                setIndex(0);
+            }}>Previous Day</button>
+            <button onClick ={() =>  {
+                setDay((day+1)%30)
+                setIndex(0);
+            }}>Next Day</button>
+            <h2>{index}</h2>
+            <h1>{(loading || isLoading ) && "Loading..."}</h1>
+            {(isSuccess) && <img src={data?.photos[index]?.img_src} alt="Error" onLoad={() => setImgLoaded(true)}/>}
+            {/*{(isSuccess) && <img src={data.photos[index]?.img_src} alt="Error" />}*/}
+            {isError && "" + error}
+        </div>
+    );
 }
+
+//***** Stolen shamelessly from https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+// function useInterval(callback, delay) {
+//     const savedCallback = useRef();
+//
+//     // Remember the latest callback.
+//     useEffect(() => {
+//         savedCallback.current = callback;
+//     }, [callback]);
+//
+//     // Set up the interval.
+//     useEffect(() => {
+//         function tick() {
+//             savedCallback.current();
+//         }
+//         if (delay !== null) {
+//             let id = setInterval(tick, delay);
+//             return () => clearInterval(id);
+//         }
+//     }, [delay]);
+// }
+//***** End of plagiarism
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {QueryClient, QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools";
 
+
+const queryClient = new QueryClient();
+console.log(process.env.NASA_KEY);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <QueryClientProvider client={queryClient}>
+          <App/>
+          <ReactQueryDevtools initialIsOpen={true}/>
+      </QueryClientProvider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
